31fbe6fefc8d8f019cfb006a3c89e6529eabe754
Matthias Mueller <matthias.mue4@gmail.com>
2022-04-24 13:55:24 +0200

    Added my own API call to OpenWeatherMap and showed Displayed current temperature (in F) in Duesseldorf

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,13 +7,34 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    // fetch('http://localhost:8081/test')
+    // .then(res => {
+    //     return res.json()
+    // })
+    // .then(function(data) {
+    //     document.getElementById('results').innerHTML = data.message
+    // })
+
+    // --- Start --- Exercise: API Call Challenge:
+    const myApiKey = '&appid=d447d3d87b60bb09c3b42f23c3d2d799&units=imperial';
+    const URLfoundation = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+    const countryCode = ',de';
+    const zip = 40239;
+    //Declare Variable
+    let date = new Date();
+
+    fetch(URLfoundation+zip+countryCode+myApiKey)
     .then(res => {
         return res.json()
     })
     .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+        // Kelvin to Celsius
+        let temperatureC = data.main.temp;
+        temperatureC = temperatureC.toFixed(2);
+        document.getElementById('results').innerHTML = `${temperatureC} this is a test`
     })
-}
+    // --- End --- Exercise: API Call Challenge:
+
+    }
 
 export { handleSubmit }
